37e6ddc0b0b1e3e9c5d85b2662ad34fc871d60a9
shivaramki <sivarama92@gmail.com>
2017-06-12 23:01:06 +0530

    my first updated commit

--- app.js
+++ app.js
@@ -4,20 +4,46 @@
 		this.products=gems;
 	});
 	app.controller("PanelController", function(){
-		this.tab=1;
+		 this.tab=1;
+		this.SelectTab=function(settab)
+		{
+this.tab=settab;
+		};
+		this.isselected=function(checktab)
+		{
+return this.tab===checktab;
+		};
 });
 
 	var gems = [{
  name: 'Dodecahedron',
  price: 2.95,
- description: 'Our most popular bottle, available in a variety of colors to help brighten up anybody’s gear. The large opening on our wide-mouth bottles easily accommodates ice cubes, fits most water purifiers and filters, and makes hand washing a breeze. The attached loop-top never gets lost and screws on and off easily. Printed graduations let keep track of your hydration. Dishwasher safe (Please make sure the top does not touch the heating element, or it will melt).',
- CanPurchase:true
+ description: 'rs to help brighten up anybody’s gear. The large opening on our wide-mouth bottles easily accommodates ice cubes, fits most water purifiers and filters, and makes hand washing a breeze. The attached loop-top never gets lost and screws on and off easily. Printed graduations let keep track of your hydration. Dishwasher safe (Please make sure the top does not touch the heating element, or it will melt).',
+ CanPurchase:true,
+ images :
+ {
+ 	full:'photogemsapphire.jpg'
+ }
+ reviews: [
+ {
+ stars: 5,
+ body: "I love this product!",
+ author: "joe@thomas.com"
+ },
+ {
+ stars: 1,
+ body: "This product sucks",
+ author: "tim@hater.com"
+ }
 },
 {
 name: 'Glass',
  price: 2.95,
  description: 'Natural Honey Facial Cleansing Bar – Enriched with natural herbal extracts. Remove dirt and impurities from your sensitive skin with our all-natural Cleansing Bar. Our gentle formula is especially designed to help neutralize skin irritations. This therapeutic blend is great for all skin types, especially sensitive or problematic skin – gentle yet effective. Directions: Wet the bar, massage into facial skin, avoiding the eye area and rinse thoroughly',
- CanPurchase:false
+ CanPurchase:false,
+ images:{
+ 	full:'3ruby.jpg'
+ }
 }
 ]
 })();
